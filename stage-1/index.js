4223197785755bb5d433f9f13a287aaeea8b4c79
Adesanya07 <gadesanya22@gmail.com>
2023-09-08 00:01:26 +0100

    ready for deployment

--- stage-1/index.js
+++ stage-1/index.js
@@ -41,7 +41,7 @@ app.get('/api', async (req, res) => {
 
 });
 
-app.listen(3000, () => console.log('Server listening on port 3000'));
+//app.listen(3000, () => console.log('Server listening on port 3000'));
 
 
   
